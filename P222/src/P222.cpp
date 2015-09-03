ddbff66b96c03efed281803ff5ae90d2c42c99e0
Violet Zhang <Violet@iceviolet.chem.upenn.edu>
2015-09-03 17:19:33 -0400

    updated src for sequence scan varying angles

--- P222/src/P222.cpp
+++ P222/src/P222.cpp
@@ -68,11 +68,13 @@ void Design(Vergil* vergil) {
 	double ref_beta = ROOM_TEMPERATURE_BETA;
 	double twobody_energy_cap = 30;
 
-	//To do
+	//file prefix
+	std::string home_dir = "/home/hzhang";
+	//std::string home_dir = "/Users/Violet/data";
 	std::string input_directory =
-			"/Users/Violet/data/peptide_design/tetramer_29/fullseq_design_approved_by_Jeff";
+			home_dir + "/peptide_design/tetramer_29/fullseq_design_approved_by_Jeff";
 	std::string output_directory =
-			"/Users/Violet/data/peptide_design/2D_lattice/C4459/P222/P222_9";
+			home_dir + "/peptide_design/2D_lattice/C4459/P222/P222_9";
 	std::string pdb_filename = input_directory
 			+ "/P222_9_X0_X12_NH2cap.pdb";
   std::string output_fileprefix = output_directory + "/C4459_P2_9";
@@ -101,14 +103,16 @@ void Design(Vergil* vergil) {
 	unfolded_compute.GenerateUnfoldedFreeEnergies();
 	Log->print_tag("RUNTIME", "Reference energy calculation: " + timer.ElapsedToString());
 
-  std::string outfile_energy = output_fileprefix + "_Gly.csv";
+  std::string outfile_energy = output_fileprefix + "_a2.74_c3.37_beta51to90.csv";
   FILE* outfile = fopen(outfile_energy.c_str(), "w");
   fprintf(outfile, "UnitCell_length_a(nm), UnitCell_length_a(nm), beta(deg), Internal_energy(Kcal/mol)\n");
   fclose(outfile);
 
-	for (double a = 24.7; a < 30.2; a += 0.1) {
-		for (double c = 29.7; c < 37.1; c += 0.1) {
-		  for (double beta = 1; beta < 91; ++beta) {
+	//for (double a = 24.7; a < 30.2; a += 0.1) {
+		//for (double c = 29.7; c < 37.1; c += 0.1) {
+  double a = 27.4;
+  double c = 33.7;
+		  for (double beta = 51; beta < 91; ++beta) {
         // Load scaffold
         std::string scaffold ="protein and backbone";
 
@@ -123,11 +127,11 @@ void Design(Vergil* vergil) {
             "NH2" };
 
         for (Domain::SectionIterator jt = vergil->domain()->SectionIterator_Begin(); jt != vergil->domain()->SectionIterator_End(); ++jt) {
-          //size_t i = 0;
+          size_t i = 0;
           for (Section::SiteIterator it = jt->SiteIterator_Begin(); it != jt->SiteIterator_End(); ++it) {
-            //it->Add(typelist[i]);
-            //++i;
-            it->Add("GLY");
+            it->Add(typelist[i]);
+            ++i;
+            //it->Add("GLY");
           }
         }
 
@@ -135,7 +139,8 @@ void Design(Vergil* vergil) {
         vergil->BuildDomain();
         vergil->TrimDomain(30.0);
 
-        //To-do: move the domain(ASU) to the correct position in the unit cell
+        //move the domain(ASU) to the correct position in the unit cell
+        //rotate ASU around x axis by 90 degree
         const Matrix m = Geometry::Transformation(Vector3(1, 0, 0), 90 * DEGREES_TO_RADIANS);
         vergil->domain()->TransformBy(m);
 
@@ -164,8 +169,8 @@ void Design(Vergil* vergil) {
 
         // Set up the problem and solve the unconstrained problem
         FunctionFreeEnergyWithReference free_energy(&energy, design_beta, &unfolded_compute);
-        //vergil->InitProbabilityProblem(free_energy);
-        //vergil->SolveProbabilityProblem();
+        vergil->InitProbabilityProblem(free_energy);
+        vergil->SolveProbabilityProblem();
 
         //Output Energy
         VectorN prob = vergil->ConformerProbabilityVector();
@@ -175,20 +180,19 @@ void Design(Vergil* vergil) {
         fclose(outfile);
 
         //Output standard files (pdb, psf, seq, csv)
-        //std::string output_file = output_directory + "/C4459_P2_9_nofixcore_a" + Log->to_str(a) + "_c" + Log->to_str(c) + "_mp_type_wCap";
-        //vergil->StandardOutput(output_file);
+        std::string output_file = output_directory + "/C4459_P2_9_nofixcore_a" + Log->to_str(a) + "_c" + Log->to_str(c) + "_mp_type_wCap";
+        vergil->StandardOutput(output_file);
 
- /*       for (Domain::SiteIterator it = vergil->domain()->SiteIterator_Begin(); it != vergil->domain()->SiteIterator_End(); ++it) {
+        for (Domain::SiteIterator it = vergil->domain()->SiteIterator_Begin(); it != vergil->domain()->SiteIterator_End(); ++it) {
           it->SetAggregatedTypeProbability();
         }
         vergil->RenameSymmetrySegname(symmetry_related_elements);
         OutputPDB output1(output_file + "_lattice.pdb", vergil->domain());
         output1.WriteLattice(symmetry_related_elements);
         Log->print("Most Probable PDB -- " + output_file + "_lattice.pdb");
-        */
 
         vergil->domain()->Clear();
 		  }
-		}
-	}
+		//}
+	//}
 }
