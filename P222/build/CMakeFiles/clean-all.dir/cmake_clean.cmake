8a8675657a6c8fd5998fc3622b23b3fb9b41f277
hzhang <hzhang@cabal.nfs>
2015-09-03 17:36:17 -0400

    untrack files in the build folder

--- P222/build/CMakeFiles/clean-all.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/clean-all"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/clean-all.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- P222/build/CMakeFiles/run_P222.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/run_P222"
-  "CMakeFiles/run_P222.dir/src/P222.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang CXX)
-  INCLUDE(CMakeFiles/run_P222.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
